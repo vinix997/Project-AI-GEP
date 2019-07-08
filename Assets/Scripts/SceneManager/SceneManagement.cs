a2d3908ccb33328da3fc32858d5afc8bf01ef75d
Chandra Delon <34028244+vinix997@users.noreply.github.com>
2019-07-08 14:33:49 +0700

    UI Main Menu

---
+++ Assets/Scripts/SceneManager/SceneManagement.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class SceneManagement : MonoBehaviour {
+
+	public GameObject optionPanel;
+	public GameObject hsPanel;
+
+	public GameObject mainPanel;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	public void Update () {
+		
+	}
+	public void Play()
+	{
+		SceneManager.LoadScene("Main");
+	}
+	public void Back()
+	{
+		SceneManager.LoadScene("Menu");
+	}
+	public void OpenOption()
+	{
+		optionPanel.SetActive(true);
+		mainPanel.SetActive(false);
+		hsPanel.SetActive(false);
+	}
+	public void OpenMain()
+	{
+		optionPanel.SetActive(false);
+		hsPanel.SetActive(false);
+		mainPanel.SetActive(true);
+	}
+	public void OpenHighScore()
+	{
+		optionPanel.SetActive(false);
+		hsPanel.SetActive(true);
+		mainPanel.SetActive(false);
+	}
+	
+	public void Exit()
+	{
+		Application.Quit();
+	}
+}
