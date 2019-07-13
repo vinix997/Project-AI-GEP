44714e8a8dd5445e0ab95aebd3ccb765b5aff961
peter andrew <peter.andrew987@gmail.com>
2019-07-13 09:27:14 +0700

    Peter : Author
    Add some Peter to Author Comment

--- Assets/Scripts/CharacterCombat.cs
+++ Assets/Scripts/CharacterCombat.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//Author : Peter Andrew
+
 [RequireComponent(typeof(CharacterStats))]
 public class CharacterCombat : MonoBehaviour {
 
